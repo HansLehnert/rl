a8bcfed4d43bec3f713c787af60076f9dcffe57d
Hans Lehnert <hans.lehnert@gmail.com>
2018-10-12 13:52:23 -0300

    Adjust training parameters and enable GPU

--- a3c/train_lab.py
+++ a3c/train_lab.py
@@ -7,14 +7,14 @@ import argparse
 import functools
 
 import estimators
-from env.simple import SimpleEnvironment as Environment
+from env.lab import LabEnvironment as Environment
 from worker import AC_Worker
 
 
 # Parse arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('level', nargs='?', default='nav_maze_random_goal_01')
-parser.add_argument('--model_dir', default='./model_a3c/')
+parser.add_argument('level', nargs='?', default='nav_corridor')
+parser.add_argument('--model_dir', default='./model_lab/')
 parser.add_argument('--t_max', type=int, default=100)
 parser.add_argument('-n', type=int)
 parser.add_argument('--viewport', action='store_true')
@@ -30,30 +30,42 @@ if n_workers is None:
 
 summary_writer = tf.summary.FileWriter(model_dir)
 
-with tf.device('/cpu:0'):
+# with tf.device('/cpu:0'):
+if True:
     global_step = tf.train.get_or_create_global_step()
 
     # Global network, to be updated by worker threads
     global_net = estimators.AC_Network('global', len(Environment.ACTIONS))
 
     # Optimizer
-    optimizer = tf.train.RMSPropOptimizer(0.0002, 0.99, 0.0, 1e-6)
+    optimizer = tf.train.RMSPropOptimizer(
+        1e-5, 0.99, 0.95, 1e-2, use_locking=True)
 
     saver = tf.train.Saver(keep_checkpoint_every_n_hours=2.0)
 
-    # Create worker graphs
+    # Create workers graphs
     workers = []
     for worker_id in range(n_workers):
+        worker_name = 'worker_{}'.format(worker_id)
+
         enable_viewport = False
         worker_summary = None
         if worker_id == 0:
             enable_viewport = args.viewport
             worker_summary = summary_writer
 
-        worker = AC_Worker(
-            name='worker_{}'.format(worker_id),
-            env=Environment(plot=enable_viewport),
+        worker_net = estimators.AC_Network(
+            name=worker_name,
+            n_out=len(Environment.ACTIONS),
             global_net=global_net,
+            optimizer=optimizer,
+            add_summary=worker_summary is not None)
+
+        worker = AC_Worker(
+            name=worker_name,
+            env=Environment(level=args.level, plot=enable_viewport),
+            global_net=global_net,
+            local_net=worker_net,
             saver=saver,
             checkpoint_dir=checkpoint_dir,
             optimizer=optimizer,
