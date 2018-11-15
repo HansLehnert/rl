36247bb3e1b89702d5149ce352b6efb3f834998d
Hans Lehnert <hans.lehnert@gmail.com>
2018-11-15 17:10:41 -0300

    Add map to lab environment

--- a3c/env/lab.py
+++ a3c/env/lab.py
@@ -20,22 +20,32 @@ class LabEnvironment():
     ):
         self.lab = deepmind_lab.Lab(
             level,
-            ['RGB_INTERLEAVED'],
+            ['RGB_INTERLEAVED', 'DEBUG.POS.TRANS'],
             {'fps': '30', 'width': '84', 'height': '84'}
         )
 
         self.plot = plot
         if self.plot:
-            plt.figure()
+            plt.figure('viewport')
             self.viewport = plt.imshow(np.zeros((84, 84)))
+            plt.figure('map')
+            self.points, = plt.plot([], [], c='g')
+            self.trajectory, = plt.plot([], [])
+            plt.axis('equal')
             plt.show(block=False)
 
         self.reset()
 
     def reset(self):
         self.lab.reset()
-        self.last_reward = 0
+        self._reward = 0
         self.step()
+
+        if self.plot:
+            plt.figure('map')
+            plt.cla()
+            self.points, = plt.plot([], [], ls='', c='g', marker='o')
+            self.trajectory, = plt.plot([], [])
 
     def step(self, action=None):
         action_vec = np.zeros((7,), dtype=np.intc)
@@ -45,29 +55,63 @@ class LabEnvironment():
         elif action == 'MOVE_BACKWARD':
             action_vec[3] = -1
         elif action == 'LOOK_LEFT':
-            action_vec[0] = -15
+            action_vec[0] = -30
         elif action == 'LOOK_RIGHT':
-            action_vec[0] = 15
+            action_vec[0] = 30
         elif action == 'STRAFE_LEFT':
             action_vec[2] = -1
         elif action == 'STRAFE_RIGHT':
             action_vec[2] = 1
 
-        self.last_reward = self.lab.step(action_vec, 4)
+        if not self.plot:
+            self._reward = self.lab.step(action_vec, 4)
 
-        if self.lab.is_running():
-            self.observations = self.lab.observations()
+            if self.lab.is_running():
+                self.observations = self.lab.observations()
+        else:
+            self._reward = 0
 
-            if self.plot:
+            for i in range(4):
+                step_reward = self.lab.step(action_vec, 1)
+                self._reward += step_reward
+
+                if not self.lab.is_running():
+                    break
+
+                self.observations = self.lab.observations()
+
+                # Update viewport
                 self.viewport.set_data(self.observations['RGB_INTERLEAVED'])
+
+                # Update trajectory
+                position = self.observations['DEBUG.POS.TRANS']
+                self.trajectory.set_xdata(
+                    np.append(self.trajectory.get_xdata(), position[0]))
+                self.trajectory.set_ydata(
+                    np.append(self.trajectory.get_ydata(), position[1]))
+
+                if step_reward == 10:
+                    self.trajectory.set_linestyle('--')
+                    self.trajectory, = plt.plot([], [])
+                elif step_reward != 0:
+                    self.points.set_xdata(
+                        np.append(self.points.get_xdata(), position[0]))
+                    self.points.set_ydata(
+                        np.append(self.points.get_ydata(), position[1]))
+
+                axes = plt.gca()
+                axes.relim()
+                axes.autoscale_view()
+
                 plt.draw()
                 plt.pause(1e-5)
+
 
     def state(self):
         return self.observations['RGB_INTERLEAVED']
 
     def reward(self):
-        return np.clip(self.last_reward, -1, 1)
+        return np.clip(self._reward, -1, 1)
 
     def done(self):
         return not self.lab.is_running()
