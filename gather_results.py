10cd86aadab2e5b64d35dfe52a674588e623348f
Hans Lehnert <hans.lehnert@gmail.com>
2019-02-01 14:37:35 -0300

    Agregar script para graficar resultados

---
+++ gather_results.py
@@ -0,0 +1,97 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats
+import os
+import collections
+import sys
+import json
+
+
+WINDOW = 10 ** 6
+STEP_LIMIT = 25 * 10 ** 6
+TAG = 'Perf/RewardTotal'
+
+with open(sys.argv[1]) as config_file:
+    plot_config = json.load(config_file)
+
+plt.figure(figsize=(8, 4))
+
+for plot_name, logs in plot_config.items():
+    print('Plotting', plot_name)
+
+    data = []
+
+    for log in logs:
+        log_name = log[0]
+        log_paths = log[1:]
+
+        print('Gathering', log_name, 'in', log_paths)
+
+        values = []
+
+        for path in log_paths:
+            for dir in os.walk(path):
+                for filename in dir[2]:
+                    if 'tfevents' not in filename:
+                        continue
+                    print('Reading', filename)
+
+                    record = tf.train.summary_iterator(
+                        os.path.join(dir[0], filename))
+
+                    try:
+                        for event in record:
+                            if event.step > STEP_LIMIT:
+                                break
+
+                            for value in event.summary.value:
+                                if value.tag == TAG:
+                                    values.append(
+                                        (event.step, value.simple_value))
+                    except tf.errors.DataLossError:
+                        print('Error, skipping', filename)
+
+        values.sort()
+
+        t, y = zip(*values)
+        mean = []
+        stdev = []
+
+        i = 0
+        for j in range(len(values)):
+            while t[j] - t[i] > WINDOW:
+                i += 1
+
+            mean.append(np.mean(y[i:j + 1]))
+            # stdev.append(np.std(y[i:j + 1]))
+            stdev.append(scipy.stats.sem(y[i:j + 1]))
+
+        data.append(
+            (log_name,
+                {'t': np.array(t),
+                    'mean': np.array(mean),
+                    'stdev': np.array(stdev),
+                }
+            )
+        )
+
+    print('Drawing')
+
+    plt.clf()
+
+    for log_name, trace in data:
+        # plt.fill_between(
+        #     data[log]['t'],
+        #     data[log]['mean'] - data[log]['stdev'],
+        #     data[log]['mean'] + data[log]['stdev'],
+        #     alpha=0.2
+        # )
+        plt.plot(trace['t'], trace['mean'], label=log_name)
+
+    plt.legend()
+    plt.xlim(0, STEP_LIMIT)
+    plt.grid()
+    plt.xlabel('Actor actions')
+    plt.ylabel('Average reward per episode')
+    plt.savefig(plot_name + '.pdf', bbox_inches='tight', format='pdf')
